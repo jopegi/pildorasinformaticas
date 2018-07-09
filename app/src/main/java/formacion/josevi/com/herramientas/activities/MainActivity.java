fc0d3b3896d6343b81cb5f585e4f4da0f539cd87
jopegi <josevi_1976@hotmail.com>
2018-07-09 19:24:43 +0200

    Primer commit app Herramientas

---
+++ app/src/main/java/formacion/josevi/com/herramientas/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package formacion.josevi.com.herramientas.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import formacion.josevi.com.herramientas.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
